b7636edad24c633a034cdd604b45fc6042911db3
AhmedShaban14 <shabanahmed491@gmail.com>
2021-11-06 15:50:15 +0000

    Enhancement Code

--- ExamSystem/Controllers/QuestionController.cs
+++ ExamSystem/Controllers/QuestionController.cs
@@ -19,16 +19,14 @@ namespace ExamSystem.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            var questions = db.Questions.ToList();
-            ViewBag.examList = questions;
+            GetListOfQuestions();
             return View();
         }
         [HttpGet]
         public ActionResult AddQuestion()
         {
             QuestionViewModel qvm = new QuestionViewModel();
-            var exams = db.Exams.ToList();
-            ViewBag.examList = new SelectList(exams, "Id", "Name");
+            GetListOfExams();
             return PartialView("_AddQuestion");
         }
         [HttpPost]
@@ -36,59 +34,32 @@ namespace ExamSystem.Controllers
         {
             if (!ModelState.IsValid)
                 return Json(new { result = 0, message = "Inserted Failed. Try Again " });
-            var question = new Question()
-            {
-                QuestionText = qvm.QuestionText,
-                Score = qvm.Score
-            };
-            db.Questions.Add(question);
-            db.SaveChanges();
-            var exam = new ExamQuestion()
-            {
-                ExamId = qvm.ExamId,
-                QuestionId = question.Id
-            };
-            db.ExamQuestions.Add(exam);
-            db.SaveChanges();
-            var answerList = new List<Answer>();
-            foreach (var ans in qvm.OptionList)
-            {
-                var answer = new Answer();
-                if (ans == qvm.QuestionAnswer)
-                    //Right Answer 
-                    answer.IsCorrect = 1;
-                else
-                    //Wrong Answer : 
-                    answer.IsCorrect = 0;
-
-                answer.QuestionId = question.Id;
-                answer.AnswerText = ans;
-                answerList.Add(answer);
-            }
-            db.Answers.AddRange(answerList);
-            db.SaveChanges();
+            //Add Question : 
+            var question = AddQuestionToDb(qvm);
+            if (question == null)
+                return Json(new { result = 2, message = "Question Inserted Failed. Try Again " });
+            //Add Exam : 
+            if (!IsExamAddedToDb(qvm, question))
+                return Json(new { result = 3, message = "Exma Inserted Failed. Try Again " });
+            //Add Answers : 
+            if (!IsAnswersAddedToDb(qvm, question))
+                return Json(new { result = 4, message = "Answer Inserted Failed. Try Again " });
             return Json(new { result = 1, message = "Inserted Successfully" });
         }
         [HttpGet]
         public ActionResult EditQuestion(int QuestionId)
         {
-            var question = db.Questions.SingleOrDefault(x => x.Id == QuestionId);
-            var exams = db.Exams.ToList();
-            ViewBag.examList = new SelectList(exams, "Id", "Name");
+            //Get Question : 
+            var question = GetQuestion(QuestionId);
+            //GetListOfExmas:
+            GetListOfExams();
             //get examID : 
-            var examId = db.ExamQuestions.SingleOrDefault(x => x.QuestionId == QuestionId).ExamId;
+            var examId = GetExmaId(QuestionId);
             //get right answer: 
-            var questionAnswer = db.Answers.SingleOrDefault(x => x.QuestionId == QuestionId && x.IsCorrect == 1).AnswerText;
+            var questionAnswer = GetQuestionAnswer(QuestionId);
             //get list of answersOptions: 
-            var options = db.Answers.Where(x => x.QuestionId == QuestionId).Select(x => x.AnswerText).ToList();
-            QuestionViewModel qvm = new QuestionViewModel()
-            {
-                QuestionText = question.QuestionText,
-                Score = question.Score,
-                ExamId = examId,
-                QuestionAnswer = questionAnswer,
-                OptionList = options
-            };
+            var options = GetAnswersText(QuestionId);
+           var qvm =  GetQuestionViewModel(question, examId, questionAnswer, options);
             return PartialView("_EditQuestion", qvm);
         }
         [HttpPost]
@@ -96,35 +67,24 @@ namespace ExamSystem.Controllers
         {
             if (!ModelState.IsValid)
                 return Json(new { result = 0, message = "Updated Failed. Try Again " });
-            var question = db.Questions.SingleOrDefault(x => x.Id == qvm.QuestionId);
-            question.QuestionText = qvm.QuestionText;
-            question.Score = qvm.Score;
-            //  db.SaveChanges();
-            var newOptionsAnswers = new List<string>();
-            newOptionsAnswers = qvm.OptionList;
-            var oldOptionsAnswers= db.Answers.Where(x => x.QuestionId == qvm.QuestionId).ToList();
-            foreach (var newOption in newOptionsAnswers.Select((newValue, i) => (newValue, i)))
-            {
-                var valueN = newOption.newValue;
-                var indexN = newOption.i;
-                oldOptionsAnswers[indexN].AnswerText = valueN;
-                if(valueN == qvm.QuestionAnswer)
-                    oldOptionsAnswers[indexN].IsCorrect = 1;
-                else
-                    oldOptionsAnswers[indexN].IsCorrect = 0;
-            }
+            //EditQuestion : 
+            if (!IsQuestionUpdatedToDb(qvm))
+                return Json(new { result = 2, message = "Question Updated Failed. Try Again " });
+            //Edit Options In Question : 
+            if (!IsOptionsOfQuestionUpdated(qvm))
+                return Json(new { result = 3, message = "Options Of Question Updated Failed. Try Again " });
             db.SaveChanges();
             return Json(new { result = 1, message = "Inserted Successfully" });
         }
         [HttpPost]
         public ActionResult DeleteQuestion(int QuestionId)
         {
-            var question = db.Questions.SingleOrDefault(x => x.Id == QuestionId);
+            var question =GetQuestion(QuestionId);
             if (question != null)
             {
-                var answers = db.Answers.Where(x => x.QuestionId == question.Id).ToList();
-                var examQuestions = db.ExamQuestions.Where(x => x.QuestionId == QuestionId).ToList();
-                var userQuestions = db.UserQuestions.Where(x => x.QuestionId == QuestionId).ToList();
+                var answers =GetAnswers(question);
+                var examQuestions = GetExamQuestions(QuestionId);
+                var userQuestions = GetUserQuestions(QuestionId);;
                 //Remove Answers : 
                 db.Answers.RemoveRange(answers);
                 //Remove ExamQuestion : 
@@ -139,5 +99,136 @@ namespace ExamSystem.Controllers
             return Json(new { result = 0 });
         }
 
+
+        public Question AddQuestionToDb(QuestionViewModel qvm)
+        {
+            var question = new Question()
+            {
+                QuestionText = qvm.QuestionText,
+                Score = qvm.Score
+            };
+            db.Questions.Add(question);
+            db.SaveChanges();
+            return question;
+        }
+        public bool IsExamAddedToDb(QuestionViewModel qvm, Question question)
+        {
+            if (qvm == null)
+                return false;
+            var exam = new ExamQuestion()
+            {
+                ExamId = qvm.ExamId,
+                QuestionId = question.Id
+            };
+            db.ExamQuestions.Add(exam);
+            db.SaveChanges();
+            return true;
+        }
+        public bool IsAnswersAddedToDb(QuestionViewModel qvm, Question question)
+        {
+            if (qvm == null)
+                return false;
+            var answerList = new List<Answer>();
+            foreach (var ans in qvm.OptionList)
+            {
+                var answer = new Answer();
+                if (ans == qvm.QuestionAnswer)
+                    //Right Answer 
+                    answer.IsCorrect = 1;
+                else
+                    //Wrong Answer : 
+                    answer.IsCorrect = 0;
+                answer.QuestionId = question.Id;
+                answer.AnswerText = ans;
+                answerList.Add(answer);
+            }
+            db.Answers.AddRange(answerList);
+            db.SaveChanges();
+            return true;
+        }
+
+        public bool IsQuestionUpdatedToDb(QuestionViewModel qvm)
+        {
+            var question = db.Questions.SingleOrDefault(x => x.Id == qvm.QuestionId);
+            if (question == null)
+                return false;
+            question.QuestionText = qvm.QuestionText;
+            question.Score = qvm.Score;
+            return true;
+        }
+        public bool IsOptionsOfQuestionUpdated(QuestionViewModel qvm)
+        {
+            if (qvm == null)
+                return false;
+            var newOptionsAnswers = new List<string>();
+            newOptionsAnswers = qvm.OptionList;
+            var oldOptionsAnswers = db.Answers.Where(x => x.QuestionId == qvm.QuestionId).ToList();
+            foreach (var newOption in newOptionsAnswers.Select((newValue, i) => (newValue, i)))
+            {
+                var valueN = newOption.newValue;
+                var indexN = newOption.i;
+                oldOptionsAnswers[indexN].AnswerText = valueN;
+                if (valueN == qvm.QuestionAnswer)
+                    oldOptionsAnswers[indexN].IsCorrect = 1;
+                else
+                    oldOptionsAnswers[indexN].IsCorrect = 0;
+            }
+            return true;
+        }
+
+        public Question GetQuestion(int QuestionId)
+        {
+
+            return db.Questions.SingleOrDefault(x => x.Id == QuestionId);
+        }
+        public string GetQuestionAnswer(int QuestionId)
+        {
+            return db.Answers.SingleOrDefault(x => x.QuestionId == QuestionId && x.IsCorrect == 1).AnswerText;
+        }
+
+        public void GetListOfQuestions()
+        {
+            var questions = db.Questions.ToList();
+            ViewBag.examList = questions;
+        }
+        public void GetListOfExams()
+        {
+            var exams = db.Exams.ToList();
+            ViewBag.examList = new SelectList(exams, "Id", "Name");
+        }
+        public int GetExmaId(int QuestionId)
+        {
+            return db.ExamQuestions.SingleOrDefault(x => x.QuestionId == QuestionId).ExamId;
+        }
+        public QuestionViewModel GetQuestionViewModel(Question question, int examId, string questionAnswer, List<string> options)
+        {
+            QuestionViewModel qvm = new QuestionViewModel()
+            {
+                QuestionText = question.QuestionText,
+                Score = question.Score,
+                ExamId = examId,
+                QuestionAnswer = questionAnswer,
+                OptionList = options
+            };
+            return qvm;
+        }
+
+        public List<string> GetAnswersText(int QuestionId)
+        {
+            return db.Answers.Where(x => x.QuestionId == QuestionId).Select(x => x.AnswerText).ToList();
+        }
+        public List<Answer> GetAnswers(Question question)
+        {
+            return db.Answers.Where(x => x.QuestionId == question.Id).ToList();
+        }
+        public List<ExamQuestion> GetExamQuestions(int QuestionId)
+        {
+            return db.ExamQuestions.Where(x => x.QuestionId == QuestionId).ToList();
+
+        }
+        public List<UserQuestion> GetUserQuestions(int QuestionId)
+        {
+            return db.UserQuestions.Where(x => x.QuestionId == QuestionId).ToList();
+        }
     }
 }
--- ExamSystem/Controllers/UserController.cs
+++ ExamSystem/Controllers/UserController.cs
@@ -5,13 +5,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 namespace ExamSystem.Controllers
 {
     [Authorize]
     public class UserController : Controller
     {
-      private ApplicationDbContext db;
+        private ApplicationDbContext db;
         public UserController()
         {
             db = new ApplicationDbContext();
@@ -20,16 +19,14 @@ namespace ExamSystem.Controllers
         public ActionResult Index()
         {
             UserQuestion uq = new UserQuestion();
-            var exams = db.Exams.ToList();
-            ViewBag.examList = new SelectList(exams, "Id", "Name");
+            GetListOfExams();
             return View(uq);
         }
         [HttpGet]
         public ActionResult ShowResult()
         {
             UserQuestion uq = new UserQuestion();
-            var exams = db.Exams.ToList();
-            ViewBag.examList = new SelectList(exams, "Id", "Name");
+            GetListOfExams();
             return View(uq);
         }
         [HttpGet]
@@ -38,22 +35,10 @@ namespace ExamSystem.Controllers
             UserQuestionViewModel uqvm = new UserQuestionViewModel();
             uqvm.UserId = 2;
             uqvm.ExamId = examId;
-            var questionIdsList = db.ExamQuestions.Where(x => x.ExamId == examId).ToList().Select(x => x.QuestionId);
-            var questionList = new List<Question>();
-            var answerList = new List<Answer>();
-            Random random = new Random();
-            foreach (var questionId in questionIdsList)
-            {
-                var question = db.Questions.SingleOrDefault(x => x.Id == questionId);
-                questionList.Add(question);
-            }
-            //Make Question List Rndom:
-            var randomQuestionList = questionList.OrderBy(x => random.Next()).Take(10).ToList();
-            foreach (var ques in randomQuestionList)
-            {
-                var answer = db.Answers.Where(x => x.QuestionId == ques.Id).ToList();
-                answerList.AddRange(answer);
-            }
+            var questionIdsList = GetListOfQuestionIds(examId);
+            var questionList = GetQuestions(questionIdsList);
+            var randomQuestionList = MakeRandomQuestions(questionList);
+            var answerList = GetAnswers(randomQuestionList);
             ViewBag.questionList = randomQuestionList;
             ViewBag.answerList = answerList;
             return View(uqvm);
@@ -63,10 +48,41 @@ namespace ExamSystem.Controllers
         {
             if (!ModelState.IsValid)
                 return Json(new { result = 0 });
-            var answer = db.Answers.SingleOrDefault(x => x.AnswerText == uqvm.AnswerText && x.QuestionId == uqvm.QuestionId);
+            var answer = GetRightAnswer(uqvm);
             if (answer == null)
                 return Json(new { result = 0 });
             var answerId = answer.Id;
+            if (!IsUserQuestionAdded(uqvm, answerId))
+                return Json(new { result = 3 });
+            return Json(new { result = 1 });
+        }
+        [HttpGet]
+        public ActionResult ShowExamsForUser(int examId)
+        {
+            var userQuestions = GetUserExam(examId);
+            if (userQuestions == null)
+                return Json(new { result = 0 }, JsonRequestBehavior.AllowGet);
+            var questionsIds = GetQuestionIds(userQuestions);
+            var answerTextList = new List<Answer>();
+            var answerQuestionList = new List<Question>();
+            var totalScore = CalcutalteUserScore(questionsIds,out answerTextList,out answerQuestionList);
+            var urvm = CreateUserResultViewModel(totalScore,answerTextList,answerQuestionList);
+            return View(urvm);
+        }
+
+        public void GetListOfExams()
+        {
+            var exams = db.Exams.ToList();
+            ViewBag.examList = new SelectList(exams, "Id", "Name");
+        }
+        public Answer GetRightAnswer(UserQuestionViewModel uqvm)
+        {
+            return db.Answers.SingleOrDefault(x => x.AnswerText == uqvm.AnswerText && x.QuestionId == uqvm.QuestionId);
+        }
+        public bool IsUserQuestionAdded(UserQuestionViewModel uqvm, int answerId)
+        {
+            if (uqvm == null || answerId == 0)
+                return false;
             var userQuestion = new UserQuestion()
             {
                 UserId = uqvm.UserId,
@@ -76,22 +92,51 @@ namespace ExamSystem.Controllers
             };
             db.UserQuestions.Add(userQuestion);
             db.SaveChanges();
-            return Json(new { result = 1 });
+            return true;
         }
-        [HttpGet]
-        public ActionResult ShowExamsForUser(int examId)
+        public IEnumerable<int> GetListOfQuestionIds(int examId)
         {
-            UserResultViewModel urvm = new UserResultViewModel();
-            var userExam = db.UserQuestions.Where(x => x.ExamId == examId && x.UserId == 2);
-            if (userExam == null)
-                return Json(new { result = 2 }, JsonRequestBehavior.AllowGet);
-            var userQuestions = db.UserQuestions.Where(x => x.UserId == 2 && x.ExamId == examId).ToList();
-            if (userQuestions == null)
-                return Json(new { result = 0 }, JsonRequestBehavior.AllowGet);
-            var questionsIds = userQuestions.Select(x => x.QuestionId);
+            return db.ExamQuestions.Where(x => x.ExamId == examId).ToList().Select(x => x.QuestionId);
+        }
+        public List<Question> GetQuestions(IEnumerable<int> questionIdsList)
+        {
+            var questionList = new List<Question>();
+            foreach (var questionId in questionIdsList)
+            {
+                var question = db.Questions.SingleOrDefault(x => x.Id == questionId);
+                questionList.Add(question);
+            }
+            return questionList;
+        }
+        public List<Question> MakeRandomQuestions(List<Question> questionList)
+        {
+            Random random = new Random();
+            //Make Question List Rndom:
+            return questionList.OrderBy(x => random.Next()).Take(10).ToList();
+        }
+        public List<Answer> GetAnswers(List<Question> randomQuestionList)
+        {
+            var answerList = new List<Answer>();
+            foreach (var ques in randomQuestionList)
+            {
+                var answer = db.Answers.Where(x => x.QuestionId == ques.Id).ToList();
+                answerList.AddRange(answer);
+            }
+            return answerList;
+        }
+        public List<UserQuestion> GetUserExam(int examId)
+        {
+            return db.UserQuestions.Where(x => x.ExamId == examId && x.UserId == 2).ToList();
+        }
+        public IEnumerable<int> GetQuestionIds(List<UserQuestion> userQuestions)
+        {
+            return userQuestions.Select(x => x.QuestionId);
+        }
+        public int CalcutalteUserScore(IEnumerable<int> questionsIds, out List<Answer> answerTextList, out List<Question> answerQuestionList)
+        {
             var totalScore = 0;
-            var answerTextList = new List<Answer>();
-            var answerQuestionList = new List<Question>();
+            answerTextList = new List<Answer>();
+            answerQuestionList = new List<Question>();
             foreach (int quesId in questionsIds)
             {
                 var question = db.Questions.SingleOrDefault(x => x.Id == quesId);
@@ -100,11 +145,16 @@ namespace ExamSystem.Controllers
                 answerTextList.AddRange(answerTexts);
                 answerQuestionList.Add(question);
             }
+            return totalScore;
+        }
+        public UserResultViewModel CreateUserResultViewModel(int totalScore, List<Answer> answerTextList, List<Question> answerQuestionList)
+        {
+            UserResultViewModel urvm = new UserResultViewModel();
             urvm.userEmail = db.UserInfos.SingleOrDefault(x => x.Id == 2).Email;
             urvm.Total = totalScore;
             urvm.userAnswerList = answerTextList;
             urvm.userQuestionList = answerQuestionList;
-            return View(urvm);
+            return urvm;
         }
     }
 }
